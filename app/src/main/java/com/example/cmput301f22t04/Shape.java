97cb2d86a700a369078c59db4ad71c1d25ef013c
Dekr0 <dekr0.dk@gmail.com>
2022-10-03 11:27:47 -0600

    Resolved conflict in Shape.java introduced in Step 3

--- app/src/main/java/com/example/cmput301f22t04/Shape.java
+++ app/src/main/java/com/example/cmput301f22t04/Shape.java
@@ -3,9 +3,5 @@ package com.example.cmput301f22t04;
 abstract public class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
-    private String color = "blue";
-=======
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> 716a4719cf63ce63b15cee3a46b7b13026efd4fc
+    String color = "Red";
 }
